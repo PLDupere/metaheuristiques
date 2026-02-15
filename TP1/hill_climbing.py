105861de9ee6c9f0c651615129be235af2bc0342
PLDupere <pierlucdup@hotmail.com>
2026-02-15 12:03:08 -0500

    use random variation into hill climbing

--- TP1/hill_climbing.py
+++ TP1/hill_climbing.py
@@ -1,11 +1,12 @@
+import random
 from helper import Helper
 from spring_design import SpringDesign
 
 class HillClimbing:
 
-    def __init__(self, step, max_iterations, neighborhood_size, stagnation_value, stagnation_iteration):
+    def __init__(self, pourcentage_variation, max_iterations, neighborhood_size, stagnation_value, stagnation_iteration):
         self.spring_design = SpringDesign()
-        self.step = step
+        self.pourcentage_variation = pourcentage_variation
         self.max_iterations = max_iterations
         self.neighborhood_size = neighborhood_size
         self.stagnation_value = stagnation_value
@@ -27,6 +28,7 @@ class HillClimbing:
                     continue
 
                 cost = self.spring_design.calcul_spring(neighbor)
+                # Keep for report
                 # if self.neighborhood_size == 1:
                 #     Helper.save_to_csv('hill_climbing', iteration, neighbor, cost)
                 # else:
@@ -50,15 +52,31 @@ class HillClimbing:
 
         return best_solution, best_cost
 
-    def __generate_neighbors(self, solution, neighborhood_size):
-        neighbors = []
-        for _ in range(neighborhood_size):
-            for i in range(len(solution)):
-                up_solution = solution.copy()
-                up_solution[i] += self.step
-                neighbors.append(up_solution)
-                down_solution = solution.copy()
-                down_solution[i] -= self.step
-                neighbors.append(down_solution)
+    # Keep for report
+    # def __generate_neighbors(self, solution, neighborhood_size):
+    #     neighbors = []
+    #     for _ in range(neighborhood_size):
+    #         for i in range(len(solution)):
+    #             up_solution = solution.copy()
+    #             up_solution[i] += self.step
+    #             neighbors.append(up_solution)
+    #             down_solution = solution.copy()
+    #             down_solution[i] -= self.step
+    #             neighbors.append(down_solution)
 
-        return neighbors
+    #     return neighbors
+
+    def __generate_neighbors(self, solution, neighborhood_size):
+            neighbors = []
+            for _ in range(neighborhood_size):
+                tmp_solution = solution.copy()
+                for i in range(len(solution)):
+                    lower_bound, upper_bound = self.spring_design.bounds[i]
+                    variation = random.uniform(-self.pourcentage_variation * (upper_bound - lower_bound), 
+                                                self.pourcentage_variation * (upper_bound - lower_bound))
+                    new_value = tmp_solution[i] + variation
+                    new_value = max(lower_bound, min(new_value, upper_bound))
+                    tmp_solution[i] = new_value
+
+                neighbors.append(tmp_solution)
+            return neighbors
