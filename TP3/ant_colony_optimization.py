8b6b1da3d5c80488202eb27c9792b99a7231ff9a
PLDupere <pierlucdup@hotmail.com>
2026-04-03 10:52:41 -0400

    add ant colony and differential evolution

--- TP3/ant_colony_optimization.py
+++ TP3/ant_colony_optimization.py
@@ -1,22 +1,68 @@
 import numpy as np
-from helper import Helper
 from spring_design import SpringDesign
 
 
 class AntColonyOptimization:
 
-    def __init__(self, max_iterations):
+    def __init__(self, iterations=100, number_ants=30, evaporation=0.8, factor=0.5):
 
         self.spring_design = SpringDesign()
-        self.max_iterations = max_iterations
+        self.iterations = iterations
+        self.number_ants = number_ants
+        self.evaporation = evaporation
+        self.factor = factor
+        self.bounds = self.spring_design.bounds
+        self.dimension = len(self.bounds)
 
+
+    def generate_solution(self):
+        return [
+            np.random.uniform(self.bounds[i][0], self.bounds[i][1])
+            for i in range(self.dimension)
+        ]
 
 
     def optimize(self):
-        best_solution = None
-        best_cost = float('inf')
 
-        for iteration in range(self.max_iterations):
-            pass
+        archive = [self.generate_solution() for _ in range(self.number_ants)]
+        archive_cost = [
+            self.spring_design.calcul_spring(x)
+            for x in archive
+        ]
+
+        best_solution = archive[np.argmin(archive_cost)]
+        best_cost = min(archive_cost)
+
+        for iteration in range(self.iterations):
+            new_archive = []
+
+            for _ in range(self.number_ants):
+                idx = np.random.randint(0, len(archive))
+                base = archive[idx]
+                new_solution = []
+
+                for i in range(self.dimension):
+                    sigma = self.factor * (self.bounds[i][1] - self.bounds[i][0])
+                    value = np.random.normal(base[i], sigma)
+                    value = np.clip(value, self.bounds[i][0], self.bounds[i][1])
+                    new_solution.append(value)
+                new_archive.append(new_solution)
+
+            new_cost = [
+                self.spring_design.calcul_spring(x)
+                for x in new_archive
+            ]
+
+            archive.extend(new_archive)
+            archive_cost.extend(new_cost)
+            sorted_index = np.argsort(archive_cost)
+            archive = [archive[i] for i in sorted_index[:self.number_ants]]
+            archive_cost = [archive_cost[i] for i in sorted_index[:self.number_ants]]
+
+            if archive_cost[0] < best_cost:
+                best_cost = archive_cost[0]
+                best_solution = archive[0]
+
+        best_solution = [float(x) for x in best_solution]
+        best_cost = float(best_cost)
         return best_solution, best_cost
-
--- TP3/differential_evolution.py
+++ TP3/differential_evolution.py
@@ -1,17 +1,59 @@
-from helper import Helper
+import numpy as np
 from spring_design import SpringDesign
 
-class DifferentialEvolution:
 
-    def __init__(self, max_iterations):
+class DifferentialEvolution:
+    def __init__(self, iterations=100, pop_size=20, mutation_factor=0.5, crossover_rate=0.7):
         self.spring_design = SpringDesign()
-        self.max_iterations = max_iterations
+        self.max_iter = iterations
+        self.bounds = np.array(self.spring_design.bounds)
+        self.pop_size = pop_size
+        self.mutation_factor = mutation_factor
+        self.crossover_rate = crossover_rate
+        self.dimension = len(self.spring_design.bounds)
+
+    def initialize(self):
+        low = self.bounds[:, 0]
+        high = self.bounds[:, 1]
+        self.population = low + (high - low) * np.random.rand(self.pop_size, self.dimension)
+
+    def mutate(self, i):
+        indexes = [index for index in range(self.pop_size) if index != i]
+        a, b, c = self.population[np.random.choice(indexes, 3, replace=False)]
+        mutant = a + self.mutation_factor * (b - c)
+        return np.clip(mutant, self.bounds[:,0], self.bounds[:,1])
+
+    def crossover(self, target, mutant):
+        cross = np.random.rand(self.dimension) < self.crossover_rate
+        if not np.any(cross):
+            cross[np.random.randint(0, self.dimension)] = True
+        trial = np.where(cross, mutant, target)
+        return trial
+
+    def select(self, target, trial):
+        if self.spring_design.calcul_spring(trial) < self.spring_design.calcul_spring(target):
+            return trial
+        return target
 
     def optimize(self):
-        best_solution = None
-        best_cost = float('inf')
+        self.initialize()
+        global_best_pos = None
+        global_best_cost = float('inf')
 
-        for iteration in range(self.max_iterations):
-            pass
+        for _ in range(self.max_iter):
+            new_pop = []
+            for i in range(self.pop_size):
+                target = self.population[i]
+                mutant = self.mutate(i)
+                trial = self.crossover(target, mutant)
+                new = self.select(target, trial)
+                new_pop.append(new)
 
-        return best_solution, best_cost
+            self.population = np.array(new_pop)
+
+        fitness = np.array([self.spring_design.calcul_spring(index) for index in self.population])
+        best_idx = np.argmin(fitness)
+        global_best_pos = self.population[best_idx]
+        global_best_cost = fitness[best_idx]
+
+        return global_best_pos, global_best_cost
